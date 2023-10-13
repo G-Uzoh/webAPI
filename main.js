c1600a2efc03d5ffadf1618c5869548fa9afb771
George Uzoh <ugochukwuuzoh@gmail.com>
2023-10-13 21:09:24 +0300

    add styles to data display cards

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+// Get DOM elements
 const generations = document.querySelectorAll('.gen');
 const search = document.querySelector('#search');
-const hint = document.querySelector('.hint');
-console.log(hint)
+const hint = document.querySelector('.text');
+const numberOfSpecies = document.querySelector('.number');
+const form = document.querySelector('form');
 const info = document.querySelector('.cards');
 
+form.classList.add('hide');
+numberOfSpecies.classList.add('hide');
 info.classList.add('hide');
 
 let pokeData = [];
 
-const fetchData = async (limit, offset) => {
+// Asynchronous function to get pokemon data
+const fetchData = async (limit, offset, genId) => {
     await
         fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
             .then(res => res.json())
@@ -31,11 +36,15 @@ const fetchData = async (limit, offset) => {
                     pokeData = res;
                     pokeCards();
             });
+
+            numberOfSpecies.textContent = `There are ${data.results?.length} Pokemon in generation ${genId}`;
+
             })
 
     pokeCards();
 }
 
+// Populate card with pokemon info
 const pokeCards = () => {
 
     const cards = pokeData?.map(pokemon => {
@@ -51,10 +60,12 @@ const pokeCards = () => {
             />
             </div>
             <div class="bottom-section">
-            <h2 class="title">${pokemon.name}</h2>
-            <p class="title">${pokemon.types?.map(type => getType(type)).join(', ')}</p>
-            <p>${pokemon.height}</p>
-            <p>${pokemon.weight}</p>
+            <h2 class="name">${pokemon.name}</h2>
+            <p class="type">${pokemon.types?.map(type => getType(type)).join(', ')}</p>
+            <div class="mass">
+                <p>${pokemon.height * 10}cm</p>
+                <p>${pokemon.weight / 10}kg</p>
+            </div>
             </div>
         </div>
             `
@@ -63,10 +74,12 @@ const pokeCards = () => {
     info.innerHTML = cards;
 }
 
+// Helper function to get pokemon type
 const getType = (type) => {
     return `${type.type.name}`;
 }
 
+// Populate cards with Pokemon data based on search criteria
 const displayPokemon = (pokemon) => {
     const searchCriteria = pokemon
         ?.map((pokemon) => {
@@ -94,6 +107,7 @@ const displayPokemon = (pokemon) => {
     info.innerHTML = searchCriteria;
 }
 
+// Implement search filter based on Pokemon name and type
 search.addEventListener('keyup', e => {
 
     
@@ -106,9 +120,10 @@ search.addEventListener('keyup', e => {
     displayPokemon(filteredCharacters);
 });
 
+// Populate cards based on selected Pokemon generation
 generations.forEach(generation => {
-    generation.addEventListener('click', function getGeneration() {
-        let genId = parseInt(generation.id);
+    generation.addEventListener('click', function getPokemonByGeneration() {
+        let genId = +generation.id;
         
         if (genId === 1) {
             limit = 151;
@@ -138,10 +153,13 @@ generations.forEach(generation => {
             limit = 112;
             offset = 906;
         }
-        fetchData(limit, offset)
-        console.log(genId, limit, offset)
+        
+        fetchData(limit, offset, genId)
+
         info.classList.remove('hide');
         hint.classList.add('hide');
+        numberOfSpecies.classList.remove('hide');
+        form.classList.remove('hide');
     });
 });
 
